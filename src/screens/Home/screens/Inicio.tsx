2850744b8e395c24b2f69441648912407567a861
Leonardo de Faveri <faver_i@hotmail.com>
2023-07-14 16:50:30 -0300

    development Inicio page

--- src/screens/Home/screens/Inicio.tsx
+++ src/screens/Home/screens/Inicio.tsx
@@ -1,8 +1,59 @@
-import {Text} from 'native-base';
+import {ScrollView, Image, Box, Text, Divider, VStack} from 'native-base';
 import React from 'react';
+import Logo from '../../../assets/Logo.png';
+import Title from '../../../components/Title';
+import InputText from '../../../components/InputText';
+import Button from '../../../components/Button';
 
 const Inicio: React.FC = () => {
-  return <Text>Inicio</Text>;
+  return (
+    <ScrollView flex={1} p={5}>
+      <VStack flex={1} mb={10}>
+        <Image source={Logo} alt="Logo Voll" />
+        <Title ml={1} alignSelf="flex-start" color="blue.500">
+          Boas-vindas!
+        </Title>
+        <Box
+          m={1}
+          p={1}
+          shadow={2}
+          borderColor="gray.200"
+          borderWidth="1"
+          rounded="lg">
+          <Box maxW="90%" alignSelf="center">
+            <InputText placeholder="Digite a especialidade" />
+            <InputText placeholder="Digite sua localização" />
+            <Button mt={3} mb={3} autoSize={true}>
+              Buscar
+            </Button>
+          </Box>
+        </Box>
+        <Title color="blue.800" mb={3}>
+          Depoimentos
+        </Title>
+        <Box m={1}>
+          <Text fontSize="md" color="#6B6E71">
+            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Eius
+            officia esse reprehenderit dolor ex delectus nostrum non, cum
+            asperiores, culpa ad fuga nobis rerum beatae quaerat minima expedita
+            consequuntur assumenda.
+          </Text>
+          <Title fontSize="lg">Leonardo, 31 anos, São Paulo/SP</Title>
+          <Divider mt={5} mb={5} />
+        </Box>
+        <Box m={1}>
+          <Text fontSize="md" color="#6B6E71">
+            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Eius
+            officia esse reprehenderit dolor ex delectus nostrum non, cum
+            asperiores, culpa ad fuga nobis rerum beatae quaerat minima expedita
+            consequuntur assumenda.
+          </Text>
+          <Title fontSize="lg">Leonardo, 31 anos, São Paulo/SP</Title>
+          <Divider mt={5} />
+        </Box>
+      </VStack>
+    </ScrollView>
+  );
 };
 
 export default Inicio;
--- src/screens/Home/screens/Perfil.tsx
+++ src/screens/Home/screens/Perfil.tsx
@@ -1,8 +1,46 @@
-import {Text} from 'native-base';
 import React from 'react';
+import {Text, ScrollView, Avatar, Divider, VStack} from 'native-base';
+import Title from '../../../components/Title';
 
 const Perfil: React.FC = () => {
-  return <Text>Perfil</Text>;
+  return (
+    <ScrollView flex={1}>
+      <VStack flex={1} alignItems="center" p={5}>
+        <Title color="blue.500">Meu Perfil</Title>
+        <Avatar
+          size="xl"
+          source={{uri: 'https://github.com/Leodf.png'}}
+          mt={5}
+        />
+        <Title color="blue.500">Informações pessoais</Title>
+        <VStack flex={1} justifyContent="center" alignItems="center">
+          <Title fontSize="lg" mb={2}>
+            Leonardo de Faveri
+          </Title>
+          <Text fontSize="md" color="#6B6E71" mb={1}>
+            17/05/1992
+          </Text>
+          <Text fontSize="md" color="#6B6E71">
+            São Paulo
+          </Text>
+        </VStack>
+
+        <Divider mt={5} />
+        <Title color="blue.500" mb={2}>
+          Histórico médico
+        </Title>
+        <Text fontSize="md" fontWeight="bold" color="#6B6E71">
+          Sinusite
+        </Text>
+        <Text fontSize="md" fontWeight="bold" color="#6B6E71">
+          Gripe
+        </Text>
+        <Text fontSize="md" fontWeight="bold" color="#6B6E71">
+          Virose
+        </Text>
+      </VStack>
+    </ScrollView>
+  );
 };
 
 export default Perfil;
